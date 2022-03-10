6d4e3adfb2f6907c3e38fb3b9eda84e8afb30b88
LorrM <lorrainemuturi@gmail.com>
2022-03-10 11:44:22 -0800

    commit4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
-
  * main - Prints "Programming is like building a multilingual
 
  *                puzzle, followed by a new line.
@@ -13,13 +12,7 @@
  */
 
 int main(void)
-  
-{
-  
+{  
   puts("\"Programming is like building a multilingual puzzle");
-  
-
-  
   return (0);
-  
 }
