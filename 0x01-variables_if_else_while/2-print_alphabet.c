a295734e4f0e1f3d18b798ec39f6cb9cdb9b3fa5
LorrM <lorrainemuturi@gmail.com>
2022-03-11 11:49:17 -0800

    commit2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,11 +5,9 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
-char ch;
+{char ch;
 for (ch = 'a'; ch <= 'z'; ch++)
-{      
-putchar(ch);    
+{putchar(ch);
 }  
 putchar('\n');
 return (0);
