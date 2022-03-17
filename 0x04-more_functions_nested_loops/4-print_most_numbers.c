764dae2929acd632b6dd2e01e8d38a4d72ba59ae
LorrM <lorrainemuturi@gmail.com>
2022-03-17 13:33:17 -0700

    commit4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,9 +4,13 @@
  */
 void print_most_numbers(void);
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-if (c != '2' && c != '4')
-_putchar(c);
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + '0');
+}
+}
 _putchar('\n');
 }
