d98e147e840cdcef7ad46e5fc12862287e08230f
LorrM <lorrainemuturi@gmail.com>
2022-03-17 19:16:12 -0700

    commit0

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_line - function that drawes a straight line in terminal
+ * @n: Integer amount of _
+ */
+void print_line(int n)
+{
+char c;
+for (c = 0; c < n; c++)
+_putchar('_');
+_putchar('\n');
+}
