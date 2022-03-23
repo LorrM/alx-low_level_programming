ca88e06554d8e53cbbb2a6a79658391c9dcb6e7d
LorrM <lorrainemuturi@gmail.com>
2022-03-23 10:25:12 -0700

    commit1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,8 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int i, j;
+i = 0;
+j = 0;
 while (dest[i] != '\0')
 i++;
 while (src[j] != '\0' && j < n)
@@ -17,6 +19,6 @@ dest[i] = src[j];
 i++;
 j++;
 }
-dest[i] != '\0';
+dest[i] = '\0';
 return (dest);
 }
