17e9566080379613f3fba13f04b9e38ac366177c
LorrM <lorrainemuturi@gmail.com>
2022-04-05 03:36:42 -0700

    commit5

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -21,9 +21,7 @@ size++;
 size++;
 }
 size++;
-/* allocate memory for total number of chars and
- * new line for each word
- */
+/* allocate memory for total number of chars and add new line for each word */
 strDup = malloc(sizeof(char) * size);
 if (strDup == NULL)
 return (NULL);
